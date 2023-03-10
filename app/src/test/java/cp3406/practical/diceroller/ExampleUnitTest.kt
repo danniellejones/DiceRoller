42ec3cb7f75bd5da640bdc8da130cb7bf769cc29
danniellejones <dannielle.jones1@my.jcu.edu.au>
2023-03-11 01:29:45 +1000

    Add jUnit test

--- app/src/test/java/cp3406/practical/diceroller/ExampleUnitTest.kt
+++ app/src/test/java/cp3406/practical/diceroller/ExampleUnitTest.kt
@@ -1,17 +1,15 @@
 package cp3406.practical.diceroller
 
+import org.junit.Assert.assertTrue
 import org.junit.Test
 
-import org.junit.Assert.*
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 class ExampleUnitTest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun generates_number() {
+        val dice = Dice(6)
+        val rollResult = dice.roll()
+        assertTrue("The value is not between 1 and 6", rollResult in 1..6)
     }
+
 }
